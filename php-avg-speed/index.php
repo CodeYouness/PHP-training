3a67a4d7270eea757d0831e7f1619f52d2642740
CodeYouness <ylijassi@gmail.com>
2024-11-13 14:35:03 +0100

    debugging of error DivisionByZeroError : Division by zero on line 17

--- php-avg-speed/index.php
+++ php-avg-speed/index.php
@@ -12,52 +12,52 @@ function calculate_speed($distance, $time) {
     } elseif($timeArray[1] === "h") {
       $timeArray[0] *= 3600;
     }
+    var_dump($timeArray);
+    var_dump($distanceArray);
+    //$result = intval($distanceArray[0]) / intval($timeArray[0]) * 2.23694;
     
-    $result = intval($distanceArray[0]) / intval($timeArray[0]) * 2.23694;
-    
-    return $result . "mph";
-  
-  }
-  
-  function separator($var) {
+    //return $result . "mph";
+}
+
+function separator($var) {
     
     $array = explode(" ", $var);
-    
+    var_dump($array);
     $num = [];
     $letters = [];
     
     foreach($array as $unit) {
-      if (is_numeric($unit)){
-        $num[] = $unit;
-      } else {
-        $letters[] = $unit;
-      }
+        if (is_numeric($unit)){
+            $num[] = $unit;
+        } else {
+            $letters[] = $unit;
+        }
     }
     
     $number = implode("", $num);
     $measure = strtolower(implode("", $letters));
-      
-      return [$number, $measure];
-  }
 
-  echo calculate_speed("22km", "5h");
+    return [$number, $measure];
+}
+
+echo calculate_speed("22km", "5h");
   
-  /*  Per la distanza:
-  esplodo la stringa e trasformo in array
-    ciclo per ogni lettera o numero e:
-      se è numero lo aggiungo alla variabile valore
-      se è lettera lo aggiungo alla variabile unità di misura
-      
-      Per la velocità ripeto la stessa cosa
-      
-    se l unita di misura della distanza è "km"
-      allora moltiplico la variabile valore per 1000
-    se l unita di misura del tempo è "m"
-      allora moltiplico la variabile tempo per 60
-    se l unita di misura del tempo è "h"
-      allora moltiplico la variabile per 3600
-      
+/*  Per la distanza:
+    esplodo la stringa e trasformo in array
+        ciclo per ogni lettera o numero e:
+            se è numero lo aggiungo alla variabile valore
+            se è lettera lo aggiungo alla variabile unità di misura
+
+    Per la velocità ripeto la stessa cosa
+
+        se l unita di misura della distanza è "km"
+            allora moltiplico la variabile valore per 1000
+        se l unita di misura del tempo è "m"
+            allora moltiplico la variabile tempo per 60
+        se l unita di misura del tempo è "h"
+            allora moltiplico la variabile per 3600
+
     infine distanza/tempo * 2.23694
     ritorno risultato + mph
-    */
+*/
     
