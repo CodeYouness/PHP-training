e9a9ccf42eae1db75640991250fc7d99d8a57704
CodeYouness <ylijassi@gmail.com>
2024-11-16 12:03:38 +0100

    added solution of excercise 5/b

---
+++ php-to-camelCase/index2.php
@@ -0,0 +1,26 @@
+<?php
+function kebabize($string) {
+    $array = str_split($string);
+    $kebabize = "";
+    foreach($array as $letter){
+        if (ctype_lower($letter)){
+            $kebabize .= strtolower($letter);
+        } elseif (ctype_upper($letter)){
+            $kebabize .= "-" . strtolower($letter);
+        }
+    }
+  
+    if (str_starts_with($kebabize, "-")){
+        $kebabize = ltrim($kebabize, "-");
+    }
+  
+    return $kebabize;
+}
+
+echo kebabize("GoToAKebab");
+
+/* trasformo stringa in array
+ciclo per ogni lettera
+    se è lettera minuscola la aggiungo alla stringa
+    se è maiuscola prima aggiungo "-" e poi la lettera ma minuscola
+*/
